e8fb230482e2a89ead5b3d2a598e2cd11f6fdd9d
MichAG100 <marek.ckib@gmail.com>
2023-02-19 11:36:34 +0100

    krok 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#krok 4 zadania z 19.02.2023
+#krok 5 zadania z 19.02.2023
 import math
 
 
@@ -14,14 +14,24 @@ class Segment:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+
+    def __str__(self) -> str:
+        return f"Segment({self.p1},{self.p2}"
+
     def distance(self):
         return math.hypot(self.p1.x-self.p2.x, self.p1.y-self.p2.y)
 
-def main():
-    p = Point(3, 0)
-    q = Point(0, 4)
-    s = Segment(p, q)
+    def svg(self):
+        return f'<line x1="{self.p1.x}" y1="{self.p1.y}" x2="{self.p2.x}" y2="{self.p2.y}" style="stroke:black" />'
 
-    print(s.distance())
+def main():
+    p = Point(300, 0)
+    q = Point(0, 400)
+    f = Segment(p, q)
+
+    print(f)
+    print(f.distance())
+    print(f.svg())
+
 
 main()

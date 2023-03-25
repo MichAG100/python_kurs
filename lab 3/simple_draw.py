ae30b2ecdec51e334596bbbdc48c2ad600c8b0de
MichAG100 <marek.ckib@gmail.com>
2023-03-25 13:29:56 +0100

    zadanie 4 krok 1

---
+++ lab 3/simple_draw.py
@@ -0,0 +1,18 @@
+#zadanie 4 krok 1
+
+import turtle
+
+def draw_rect():
+    print("XXX")
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+
+    screen.exitonclick()

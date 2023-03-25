6330851bd4c80817e83f8e50eabed251c72d7f18
MichAG100 <marek.ckib@gmail.com>
2023-03-25 14:03:24 +0100

    zadanie 4 krok 1,2,3

--- lab 3/simple_draw.py
+++ lab 3/simple_draw.py
@@ -6,6 +6,7 @@ def draw_rect():
     print("XXX")
     screen = turtle.Screen()
     t = turtle.Turtle()
+    t.speed(1)
 
     t.forward(50)
     t.right(90)
@@ -16,3 +17,31 @@ def draw_rect():
     t.forward(50)
 
     screen.exitonclick()
+
+#zadanie 4 krok 3
+
+def draw_star():
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+    t.speed(1)
+
+    for _ in range(5):
+        t.forward(100)
+        t.right(144)
+    screen.exitonclick()
+
+#zadanie 4 krok 2
+
+def draw_reg_poly(n):
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+    t.speed(1)
+
+    for _ in range (n):
+        t.forward(100)
+        t.left(360 / n)
+
+    screen.exitonclick()
+
+
+

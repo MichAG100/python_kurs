5879889f013ab309082b71cc4a793a47eb51fe5d
MichAG100 <marek.ckib@gmail.com>
2023-03-26 13:53:34 +0200

    lab 4 zadanie 3 krok  1

---
+++ Lab4/zad3/mainwidget.py
@@ -0,0 +1,18 @@
+# lab4 zadanie3 krok1
+from PySide2.QtWidgets import *
+from PySide2.QtGui import*
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(500, 500)
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        brush = QBrush()
+        brush.setColor(QColor("white"))
+        brush.setStyle(Qt.SolidPattern)
+        print(self.rect())
+        painter.fillRect(self.rect(), brush)
+        painter.end()

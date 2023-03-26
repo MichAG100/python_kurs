3dffbfb497fc48643fa5c65ab45f6efc8294d701
MichAG100 <marek.ckib@gmail.com>
2023-03-26 09:08:11 +0200

    zadanie

--- lab 3/reverse_polish_notation..py
+++ lab 3/reverse_polish_notation..py
@@ -1,17 +1,14 @@
-#zadanie 2 krok 1
 
-#import reverse_polish_notation as rpn
 
 import random
 
 #from reverse_polish_notation import *
 
-from liczba_pi import approximate_pi
+from monte_carlo import approximate_pi
 
 def main():
     #print(rvp("((2+7)/3+(8-3)*4)/2"))
     #print(solve("2 7 + 3 / 14 3 - 4 * + 2 /"))
     print(approximate_pi())
 
-#if __name__=="__main__":
-    #main()
+main()

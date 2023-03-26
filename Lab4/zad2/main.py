f0280f8c6c32c4e4c33635f696f833056f4f028a
MichAG100 <marek.ckib@gmail.com>
2023-03-26 12:33:21 +0200

    zadanie 2 krok  3

---
+++ Lab4/zad2/main.py
@@ -0,0 +1,16 @@
+#main
+
+
+from PySide2.QtWidgets import QApplication
+from mainwindow import MainWindow
+def main():
+    app = QApplication([])
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()

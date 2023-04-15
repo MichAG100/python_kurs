443299985ffd5e6930342f09726684c9d7cc223b
MichAG100 <marek.ckib@gmail.com>
2023-04-15 12:15:51 +0200

    egzamin krok 4

--- Egzamin/main.py
+++ Egzamin/main.py
@@ -1,8 +1,14 @@
 from produkt import Produkt
+from koszyk import Koszyk
 
 def main():
     Produkt.wczytaj_produkty("ceny.csv")
-    print(Produkt.produkt(Chleb pszenno-żytni - za 0.5 kg).cena(1, 2021))
+   # print(Produkt.produkt("Chleb pszenno-żytni - za 0.5 kg").cena(1, 2021))
+    koszyk = Koszyk()
+    koszyk.dodaj(Produkt.produkt("Cebula - za 1 kg"))
+    koszyk.dodaj(Produkt.produkt("Cebula - za 1 kg"))
+    pass
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     main()

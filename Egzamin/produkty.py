4d9fc94acfc92b5af6fb3d8ad82426f862ab2713
MichAG100 <marek.ckib@gmail.com>
2023-04-15 10:46:37 +0200

    egzamin krok 1 i 2

--- Egzamin/produkty.py
+++ Egzamin/produkty.py
@@ -1,7 +1,8 @@
 class Produkty:
-    def __init__(self, nazwa, cena):
-        self.__nazwa = nazwa
-        self.__cena = cena
+    def __init__(self,wiersz):
+        dane = wiersz.split(",")
+        self.__nazwa = dane [0]
+        self.__ceny =[float(cena) for cena in dane[1:]]
 
     def get_nzawa(self):
         return self.__nazwa
